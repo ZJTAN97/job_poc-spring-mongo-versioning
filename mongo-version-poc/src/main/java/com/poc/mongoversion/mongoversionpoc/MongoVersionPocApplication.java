4972a269dd7921f180b661cd10fe8c4276f56d19
ZJ <zhijietan97@gmail.com>
2022-10-18 16:15:18 +0800

    add: starter spring boot application

---
+++ mongo-version-poc/src/main/java/com/poc/mongoversion/mongoversionpoc/MongoVersionPocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.mongoversion.mongoversionpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoVersionPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoVersionPocApplication.class, args);
+	}
+
+}
---
+++ mongo-version-poc/src/test/java/com/poc/mongoversion/mongoversionpoc/MongoVersionPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.mongoversion.mongoversionpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoVersionPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
